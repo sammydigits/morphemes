12994bd2d3a991a7bc8e16d03b8a07dd18b15f3c
Sam Thompson <sam.thompson@hey.com>
2024-03-30 13:48:13 -0400

    fix typo

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,8 +4,8 @@ export default function AboutPage() {
   return (
     <div>
       <h1 className={title()}>About</h1>
-      <p className="mt-8">
-        Information and morphemes and Literacy Lab here, along with contact
+      <p className="mt-8 py-4">
+        Information about morphemes and Literacy Lab here, along with contact
         details.
       </p>
     </div>
